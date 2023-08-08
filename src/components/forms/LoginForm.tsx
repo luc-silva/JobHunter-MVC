81f5fdeedd2070c57a75455feabe310d16e8606d
Lucas Santos <lucas.silva1203@outlook.com>
2023-08-08 18:04:47 -0300

    fix: add missing html input placeholders

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -3,11 +3,11 @@ export const LoginForm = () => {
     <form action="" className="login-form">
       <div className="login-form__input-container">
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" placeholder="josh@user.com"/>
       </div>
       <div className="login-form__input-container">
         <label htmlFor="password">Senha:</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" placeholder="*******"/>
       </div>
       <div className="login-form__submit-container">
         <input type="submit" value="Entrar" />
